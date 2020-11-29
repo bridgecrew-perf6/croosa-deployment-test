2defdb8bba0755bc4a63287f7ce98e12d817e71e
Vitor Rodrigues <vitorr661@gmail.com>
2020-11-29 10:25:02 -0300

    pre-lancamento

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <html className={styles.main}>
+    <div className={styles.container}>
+      <h1>Croosa</h1>
+      <h3>Em breve.</h3>
+    </div>
+    </html>
+  )
+}
